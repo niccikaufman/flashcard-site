c977acdead60625b78b2acf36df00294c00ab9f7
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-17 18:19:24 -0700

    add definition toggling

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
   $(".card-btn").click(function(){
     $(".card-hidden").toggle(); 
     $(".card-showing").toggle();
-
   });
 });
 
+$(document).ready(function(){
+  $(".def-btn").click(function(){
+    $(".card-hidden").toggle(); 
+    $(".card-showing").toggle();
+  });
+});
 
 
