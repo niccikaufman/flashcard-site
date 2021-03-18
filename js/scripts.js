89ddc4061631b3b31aa6566b44f8e0d4bbb82193
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-17 18:38:19 -0700

    add id tags for each card so they toggle independently of others

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,111 @@
 $(document).ready(function(){
-  $(".card-btn").click(function(){
-    $(".card-hidden").toggle(); 
-    $(".card-showing").toggle();
+  $("#js-btn").click(function(){
+    $("#js-hidden").toggle(); 
+    $("#js-showing").toggle();
   });
 });
 
 $(document).ready(function(){
-  $(".def-btn").click(function(){
-    $(".card-hidden").toggle(); 
-    $(".card-showing").toggle();
+  $("#js-def-btn").click(function(){
+    $("#js-hidden").toggle(); 
+    $("#js-showing").toggle();
   });
 });
 
+$(document).ready(function(){
+  $("#op-btn").click(function(){
+    $("#op-hidden").toggle(); 
+    $("#op-showing").toggle();
+  });
+});
 
+$(document).ready(function(){
+  $("#op-def-btn").click(function(){
+    $("#op-hidden").toggle(); 
+    $("#op-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#var-btn").click(function(){
+    $("#var-hidden").toggle(); 
+    $("#var-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#var-def-btn").click(function(){
+    $("#var-hidden").toggle(); 
+    $("#var-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#mth-btn").click(function(){
+    $("#mth-hidden").toggle(); 
+    $("#mth-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#mth-def-btn").click(function(){
+    $("#mth-hidden").toggle(); 
+    $("#mth-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#arg-btn").click(function(){
+    $("#arg-hidden").toggle(); 
+    $("#arg-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#arg-def-btn").click(function(){
+    $("#arg-hidden").toggle(); 
+    $("#arg-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#para-btn").click(function(){
+    $("#para-hidden").toggle(); 
+    $("#para-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#para-def-btn").click(function(){
+    $("#para-hidden").toggle(); 
+    $("#para-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#rtn-btn").click(function(){
+    $("#rtn-hidden").toggle(); 
+    $("#rtn-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#rtn-def-btn").click(function(){
+    $("#rtn-hidden").toggle(); 
+    $("#rtn-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#str-btn").click(function(){
+    $("#str-hidden").toggle(); 
+    $("#str-showing").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#str-def-btn").click(function(){
+    $("#str-hidden").toggle(); 
+    $("#str-showing").toggle();
+  });
+});
